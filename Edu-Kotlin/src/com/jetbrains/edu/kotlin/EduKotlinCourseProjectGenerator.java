86c141b562bcd7d7213d82b8f355fdd7a765cb0c
Liana.Bakradze <liana.bakradze@jetbrains.com>
2017-09-21 12:31:20 +0300

    EDU-1047 Generate gradle project structure

--- Edu-Kotlin/src/com/jetbrains/edu/kotlin/EduKotlinCourseProjectGenerator.java
+++ Edu-Kotlin/src/com/jetbrains/edu/kotlin/EduKotlinCourseProjectGenerator.java
@@ -4,11 +4,16 @@ import com.intellij.facet.ui.ValidationResult;
 import com.intellij.ide.util.newProjectWizard.AbstractProjectWizard;
 import com.intellij.ide.util.newProjectWizard.StepSequence;
 import com.intellij.ide.util.projectWizard.ProjectBuilder;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ui.configuration.actions.NewModuleAction;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.platform.DirectoryProjectGenerator;
+import com.jetbrains.edu.learning.StudyTaskManager;
+import com.jetbrains.edu.learning.core.EduUtils;
+import com.jetbrains.edu.learning.intellij.generation.EduGradleModuleGenerator;
 import com.jetbrains.edu.learning.intellij.generation.EduIntellijCourseProjectGeneratorBase;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
@@ -16,8 +21,11 @@ import org.jetbrains.annotations.Nullable;
 import org.jetbrains.kotlin.idea.KotlinIcons;
 
 import javax.swing.*;
+import java.io.IOException;
 
 class EduKotlinCourseProjectGenerator extends EduIntellijCourseProjectGeneratorBase {
+
+  private static final Logger LOG = Logger.getInstance(EduKotlinCourseProjectGenerator.class);
 
   @NotNull
   @Override
@@ -38,6 +46,18 @@ class EduKotlinCourseProjectGenerator extends EduIntellijCourseProjectGeneratorB
 
       @Override
       public void generateProject(@NotNull Project project, @NotNull VirtualFile baseDir, @Nullable Object settings, @NotNull Module module) {
+        if (EduUtils.isAndroidStudio()) {
+          ApplicationManager.getApplication().runWriteAction(() -> {
+            try {
+              StudyTaskManager.getInstance(project).setCourse(myCourse);
+              myCourse.initCourse(false);
+              EduGradleModuleGenerator.createCourseContent(project, myCourse, baseDir.getPath());
+            } catch (IOException e) {
+              LOG.error("Failed to generate course", e);
+            }
+          });
+          return;
+        }
         new NewModuleAction().createModuleFromWizard(project, null, new AbstractProjectWizard("", project, baseDir.getPath()) {
           @Override
           public StepSequence getSequence() {
